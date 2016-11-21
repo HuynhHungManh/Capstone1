600ca7fcbe5e65694438417fb4129f74d8aafa37
manhhung <t2cup2t1@gmail.com>
2016-11-21 22:12:22 +0700

    deploy 10h

--- app/src/scripts/user/profile_user/education/update.education/update.education.controller.js
+++ app/src/scripts/user/profile_user/education/update.education/update.education.controller.js
@@ -2,7 +2,7 @@
  * Created by PC on 10/12/2016.
  */
 angular.module('myApp')
-    .controller('UpdateEducationController' ,function ($scope,UpdateEducationService,$stateParams, $filter, $cookieStore,
+    .controller('UpdateEducationController', function ($scope, UpdateEducationService, $stateParams, $filter, $cookieStore,
                                                        $rootScope) {
         $rootScope.doTheBack = function () {
             window.history.back();
@@ -14,11 +14,18 @@ angular.module('myApp')
                 $scope.user = $filter('filter')(res.data, {username: $stateParams.username})[0];
                 $scope.education = $scope.user.educations[0];
 
+                if ($scope.education.name_of_school == undefined ||
+                    $scope.education.address_of_school == undefined ||
+                    $scope.education.describe == undefined || $scope.education.major == undefined ||
+                    $scope.education.achiviement == undefined) {
+                    alert("Please enter education.. !");
+                }
+
                 $scope.editEdu = function () {
                     $scope.updateEdu.personalId = $scope.user.id;
                     $scope.updateEdu.created_at = new Date();
                     $scope.updateEdu.updated_at = new Date();
-                    if($scope.education == undefined) {
+                    if ($scope.education == undefined) {
                         UpdateEducationService.createEducation($scope.updateEdu, $scope.data.access_token)
                             .then(function () {
                                 $scope.updateEdu = {};
@@ -28,8 +35,8 @@ angular.module('myApp')
                                 alert("Create failed !");
                             });
                     }
-                    else{
-                        UpdateEducationService.updateEducation($scope.updateEdu,$scope.education.id, $scope.data.access_token)
+                    else {
+                        UpdateEducationService.updateEducation($scope.updateEdu, $scope.education.id, $scope.data.access_token)
                             .then(function () {
                                 $scope.updateEdu = {};
                                 $window.location.reload();
